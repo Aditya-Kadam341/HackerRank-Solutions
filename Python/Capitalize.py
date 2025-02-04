7dfb7d4091ded6c6ced1896a688f1daf86e3fda1
Aditya-Kadam341 <kadamaditya341@gmail.com>
2025-02-04 17:07:33 +0100

    Capitalize

---
+++ Python/Capitalize.py
@@ -0,0 +1,13 @@
+s = "chris alan"
+a=s.split(" ")
+s=""
+for i in (a):
+    s=s+i.capitalize()+ " "
+
+print(s)
+
+
+
+###### OR ######
+
+#print(s.title()) #fails with test case 2 but works for the rest
