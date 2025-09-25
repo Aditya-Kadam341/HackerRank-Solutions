e39c902b80a6a77a5456db1969c4205ed2738c2c
Aditya-Kadam341 <kadamaditya341@gmail.com>
2025-09-25 15:14:33 +0200

    FindAngle

---
+++ Python/Medium/FindAngleMBC.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math 
+AB=float(input())
+BC=float(input())
+print(str(int(round(math.degrees(math.atan2(AB,BC)))))+"\u00B0")
