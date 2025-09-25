158ef4666993c14b31ebae89974d1687892de986
Aditya-Kadam341 <kadamaditya341@gmail.com>
2025-09-25 15:08:57 +0200

    TimeDelta update

--- Python/Medium/TimeDelta.py
+++ Python/Medium/TimeDelta.py
@@ -7,25 +7,8 @@ import re
 import sys
 from datetime import datetime as dt
 # Complete the time_delta function below.
-def time_delta(t1, t2):
-    date_format = '%a %d %b %Y %H:%M:%S %z'
-    for i in range(int(input())):
-        print(int(abs((dt.strptime(input(),date_format)-
-        dt.strptime(input(),date_format)).total_seconds())))
-    return
+date_format = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(),date_format)-
+    dt.strptime(input(),date_format)).total_seconds())))
 
-if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input())
-
-    for t_itr in range(t):
-        t1 = input()
-
-        t2 = input()
-
-        delta = time_delta(t1, t2)
-
-        # fptr.write(delta + '\n')
-
-    # fptr.close()
